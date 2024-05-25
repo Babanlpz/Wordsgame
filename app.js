2c696e5fb4ee6478bce2afb25c05c046050c9e61
Esteban <baab2an@live.fr>
2024-05-25 22:36:26 +0200

    FEAT function pour le timer

--- app.js
+++ app.js
@@ -5,3 +5,53 @@ const btnValide = document.querySelector(".valide");
 const input = document.querySelector("input");
 const chrono = document.querySelector(".chrono-content");
 const scoreDisplay = document.querySelector(".score");
+
+let motCorrect;
+let score = 0;
+let questionIndex = 0;
+let timer;
+
+const initTimer = (maxTime) => {
+  clearInterval(timer);
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      chrono.innerText = maxTime;
+    } else {
+      handleTimerEnd();
+    }
+  }, 1000);
+};
+
+const handleTimerEnd = () => {
+  clearInterval(timer);
+  questionIndex++;
+  if (questionIndex < words.length) {
+    initGame();
+  } else {
+    displayScore();
+  }
+};
+
+const initGame = () => {
+  initTimer(20);
+
+  let randomObj = mots[questionIndex];
+  let motTableau = randomObj.mot.split("");
+
+  for (let i = motTableau.length - 1; i > 0; i++) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [motTableau[i], motTableau[j]] = [motTableau[j], motTableau[i]];
+  }
+
+  motText.innerText = motTableau.join("");
+  indiceText.innerText = randomObj.indice;
+
+  motCorrect = randomObj.mot.toLowerCase();
+
+  input.value = "";
+
+  input.setAttribute("maxLength", motCorrect.length);
+};
+
+initGame();
