9f60bcb499203585eae2d6c143c5569aa62784fe
Esteban <baab2an@live.fr>
2024-05-25 22:26:22 +0200

    FEAT app.js const

---
+++ app.js
@@ -0,0 +1,7 @@
+const motText = document.querySelector(".mot");
+const indiceText = document.querySelector(".indice-content");
+const btnRefresh = document.querySelector(".refresh");
+const btnValide = document.querySelector(".valide");
+const input = document.querySelector("input");
+const chrono = document.querySelector(".chrono-content");
+const scoreDisplay = document.querySelector(".score");
